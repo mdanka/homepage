7d8907a17586e20279b9ad5c6f7e47b803c88dc3
Miklos Danka <danka.miklos@gmail.com>
2018-09-23 21:54:57 +0200

    Added icons to the top section

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faRss } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export class Overview extends React.PureComponent<{}, {}> {
     public render() {
@@ -8,15 +11,21 @@ export class Overview extends React.PureComponent<{}, {}> {
                 <div className="hp-overview-phonetics">Miklós András Danka - [miklɔːʃ ɒndrɑːʃ dɒnkɒ]</div>
                 <div className="hp-overview-links">
                     <a href="http://blog.miklosdanka.com" target="_blank">
+                        <FontAwesomeIcon className="hp-overview-icon" icon={faRss} />
                         Blog
                     </a>
-                    <a href="https://github.com/mdanka/" target="_blank">
-                        Github
-                    </a>
                     <a href="https://www.linkedin.com/in/mikl%C3%B3s-andr%C3%A1s-danka-19415147/" target="_blank">
+                        <FontAwesomeIcon className="hp-overview-icon" icon={faLinkedin} />
                         LinkedIn
                     </a>
-                    <a href="mailto:danka.miklos+homepage[AT]gmail.com">Email</a>
+                    <a href="https://github.com/mdanka/" target="_blank">
+                        <FontAwesomeIcon className="hp-overview-icon" icon={faGithub} />
+                        Github
+                    </a>
+                    <a href="mailto:danka.miklos+homepage[AT]gmail.com">
+                        <FontAwesomeIcon className="hp-overview-icon" icon={faEnvelope} />
+                        Email
+                    </a>
                 </div>
             </div>
         );
